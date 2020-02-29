6ed66cf6bdc425873b3618fbab1a1cd4467a5de0
Matthew Holmes <matt_holmes123@hotmail.com>
2020-02-29 10:35:15 +0000

    Complete "enums" folder exercise.

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -1,15 +1,17 @@
 // enums2.rs
 // Make me compile! Execute `rustlings hint enums2` for hints!
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
-    // TODO: define the different variants used below
+    Move { x: i32, y: i32 },
+    Echo(String),
+    ChangeColor(i32, i32, i32),
+    Quit
 }
 
 impl Message {
     fn call(&self) {
+        println!("********************");
         println!("{:?}", &self);
     }
 }
@@ -24,5 +26,29 @@ fn main() {
 
     for message in &messages {
         message.call();
+
+        // Playing around with matches
+        match message {
+            Message::Move {x, y} => {
+                println!("{:?}", x);
+                println!("{:?}", y);
+            }
+            Message::Echo(msg) => {
+                println!("{:?}", msg);
+            }
+            Message::ChangeColor(r, g, b) => {
+                println!("RGB: {:?} {:?} {:?}", r, g, b);
+            }
+            Message::Quit => {
+                println!("Better be off then.")
+            }
+        }
+
+        // Playing around with if let.
+        if let Message::Move {x, y} = message {
+            println!("This is if let matching Message::Move");
+            println!("{:?}", x);
+            println!("{:?}", y);
+        }
     }
 }
