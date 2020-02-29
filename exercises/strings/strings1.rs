9e7c5221904b3351a0263e774d59233eda802335
Matthew Holmes <matt_holmes123@hotmail.com>
2020-02-29 09:14:35 +0000

    Complete "strings" folder exercises.

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -2,13 +2,14 @@
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings1` for hints ;)
 
-// I AM NOT DONE
-
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {}", answer);
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    // just to remind me that &str is a string slice.
+    let blue_string_slice = "blue";
+
+    blue_string_slice.to_string()
 }
