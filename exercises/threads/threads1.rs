cd221904044e7e241a3d7d94f7c7c06545aabc59
Matthew Holmes <matt_holmes123@hotmail.com>
2020-04-02 08:14:22 +0100

    Complete threads1.rs

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -1,13 +1,11 @@
 // threads1.rs
 // Make this compile! Execute `rustlings hint threads1` for hints :)
-// The idea is the thread spawned on line 21 is completing jobs while the main thread is
+// The idea is the thread spawned on line 25 is completing jobs while the main thread is
 // monitoring progress until 10 jobs are completed. If you see 6 lines
-// of "waiting..." and the program ends without timing out when running,
+// of "waiting..." and the program ends without tinds without ming out when running,
 // you've got it :)
 
-// I AM NOT DONE
-
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -16,15 +14,26 @@ struct JobStatus {
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+
+    // Arc so its sharable between threads
+    // Mutex to lock access to value inside so we can mutate the value
+    // needed because arc is shared access to *immutable* data.
+    let status = Arc::new(
+        Mutex::new(
+            JobStatus { jobs_completed: 0 }
+        )
+    );
     let status_shared = status.clone();
+
     thread::spawn(move || {
         for _ in 0..10 {
             thread::sleep(Duration::from_millis(250));
+            let mut status_shared = status_shared.lock().unwrap();
             status_shared.jobs_completed += 1;
         }
     });
-    while status.jobs_completed < 10 {
+
+    while status.lock().unwrap().jobs_completed < 10 {
         println!("waiting... ");
         thread::sleep(Duration::from_millis(500));
     }
